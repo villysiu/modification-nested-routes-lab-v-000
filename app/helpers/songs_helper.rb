ed63e997415067f2d6bf24dfa8153e171f2852f3
Villy Siu <villysiu@gmail.com>
2017-11-08 19:20:45 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,6 +1,6 @@
 module SongsHelper
   def artist_select(artist, song)
-    if artist.nil?
+    if artist.empty?
       select :artist_id, options_from_collection_for_select(Artist.all, :id, :name)
     else
       text_field(:song, :artist, :value=> artist.name)
