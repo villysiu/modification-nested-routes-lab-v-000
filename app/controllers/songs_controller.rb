d2cd9e1b9d8dff026a20e302026c6051c3fd2b18
Villy Siu <villysiu@gmail.com>
2017-11-08 17:59:13 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,7 +39,19 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        @song = artist.songs.find_by(id: params[:id])
+        if @song.nil?
+          redirect_to artist_songs_path(artist), alert: "Song not found." 
+        end
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
