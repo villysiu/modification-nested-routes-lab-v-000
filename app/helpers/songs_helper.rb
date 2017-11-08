910cefc21378da94f42401d9e3b3ae616bcc023a
Villy Siu <villysiu@gmail.com>
2017-11-08 18:37:56 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
     if artist.nil?
       select :artist_id, options_from_collection_for_select(Artist.all, :id, :name)
     else
-      text_field :song, song.artist_id
+      text_field :song, "song.artist_id"
     end
   end
 end
