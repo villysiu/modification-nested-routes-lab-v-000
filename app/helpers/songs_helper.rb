2903d7dbe5766a01a6558ed2e80a8bf55e2bccd5
Villy Siu <villysiu@gmail.com>
2017-11-08 20:01:44 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
+  def artist_select(artist, song)
+    if artist.nil?
+      select_tag :song_artist_id, options_from_collection_for_select(Artist.all, :id, :name)
 
+    else
+      text_field(:song, :artist, :value=> artist.name)
+    end
+  end
 end
